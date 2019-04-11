6d2c17ec9e43e953871e168a19b27d2ae04d261a
Paul Grogan <paulgrogan@gmail.com>
2019-04-11 13:42:54 -0400

    add simple setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+import sys
+from setuptools import setup
+
+setup(
+    name='ofspy',
+    version='1.1',
+    description='Orbital Federates Simulation (Python)',
+    author='Paul T. Grogan',
+    author_email='pgrogan@stevens.edu',
+    packages=['ofspy']
+)
